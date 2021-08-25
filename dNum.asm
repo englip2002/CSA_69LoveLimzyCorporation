0d27298a5140db5a31bce088c7ae905a921183b2
AlpacaMoon <66011139+AlpacaMoon@users.noreply.github.com>
2021-08-25 21:42:38 +0800

    DisplayNumber Function (Byte & Word only)
    Can display byte and word in numeric form
    To use:
    Move the number you want to output to AX (MOV AX, 123)
    Call the DisplayNum function (CALL DisplayNum)

---
+++ dNum.asm
@@ -0,0 +1,53 @@
+; Function to display number (up to dw, dd still struggling)
+
+.MODEL SMALL
+.STACK 100
+.DATA
+    n1 DD 1
+    n2 DW 120
+    tenB DB 10
+    tenW DW 10
+    displayStack DW 5 DUP (0)
+    displayStackIndex DW 0
+    newline DB 13, 10, "$"
+.CODE
+MAIN PROC
+    MOV AX, @DATA
+    MOV DS, AX
+
+    ; How to use:
+    ; Move the number to AX
+    ; Call DisplayNum
+    MOV AX, n2
+    CALL DisplayNum
+
+    MOV AX, 4C00H
+    int 21h
+MAIN ENDP
+
+; Max display: 65535 (1 word)
+DisplayNum PROC
+    MOV displayStackIndex, 0
+    displayIntLoop:
+        MOV DX, 0
+        DIV tenW
+        MOV BX, displayStackIndex
+        MOV displayStack[BX], DX
+        INC displayStackIndex
+        CMP AX, 0
+        JNE displayIntLoop
+
+    doneLoop:
+        DEC displayStackIndex
+        DisplayDecimal:
+        MOV BX, displayStackIndex
+        MOV DX, displayStack[BX]
+        MOV AH, 02H
+        ADD DL, 30H
+        INT 21H
+        CMP displayStackIndex, 0
+        JNE doneLoop
+    RET
+DisplayNum ENDP
+
+END MAIN
