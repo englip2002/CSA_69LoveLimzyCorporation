670d58c0b7aa5bde153b7e9dbe5d973163c2e391
AlpacaMoon <66011139+AlpacaMoon@users.noreply.github.com>
2021-08-26 21:58:09 +0800

    Smol changes

--- AOpt4.asm
+++ AOpt4.asm
@@ -1,7 +1,11 @@
-; Assignment Option 1
+; Assignment Option 4
 ; Author: Thong So Xue
-; Description:
-;   Loop through all the products and display their product name, description, price and quantity in stock
+; Description: 
+;   1) Enter product ID
+;   2) Display current product info
+;   3) Choose which field to alter
+;   4) Enter new changes
+;   5) Display new changes
 
 .MODEL SMALL
 .STACK 100
@@ -16,14 +20,17 @@
     prodDescs3 DB "Red roses (18 stalks)                             Hydrangea (3 stalks)                              Pink tulips (20 stalks)                           Mix of Roses, Spray roses, Eustomas, and Ping Pong$"
     prodPrices DW 50, 198, 98, 128, 48, 108, 128, 118, 208, 98, 298, 298
     prodQuantities DB 50, 20, 25, 30, 35, 20, 25, 15, 15, 20, 15, 10
-    opt1Title DB "( Option 1 ) Display Item Information", 13, 10, 10, "$" 
     newline DB 13, 10, "$"
-    anyKeyToContinue DB "< Press any key to continue >$"
     displayPrefixID    DB "Product ID       : $"
     displayPrefixName  DB "Product Name     : $"
     displayPrefixDesc  DB "Description      : $"
     displayPrefixPrice DB "Price            : RM $"
     displayPrefixQty   DB "Quantity In Stock: $"
+
+    opt4Title DB "( Option 4 ) Update Item Information", 13, 10, 10, "$" 
+    opt4GetProdID DB "Enter the product's ID to update (01-12): $"
+    opt4InvalidID DB "Invalid product ID! Please re-enter. $"
+    opt4ProdID DB ?
 
     currProdIndex DB 0
     currProdNameIndex DB 0
@@ -37,13 +44,52 @@ MAIN PROC
     MOV DS, AX
 
     MOV AH, 09H
-    LEA DX, opt1Title
+    LEA DX, opt4Title
     INT 21H
 
+    ; Input product ID
+    Opt4InputIDLoop:
+        MOV AH, 09H
+        LEA DX, opt4GetProdID
+        INT 21H
+        MOV AH, 01H
+        INT 21H
+        SUB AL, 30H
+        MOV BL, AL
+        INT 21H
+        SUB AL, 30H
+        MOV BH, AL
+        MOV AL, BL
+        MOV CL, 10
+        MUL CL
+        ADD AL, BH
+        CMP AL, 0
+        JB Opt4InvalidInputID
+        CMP AL, 12
+        JA Opt4InvalidInputID
+        JMP Opt4DisplayInfoPre
     
-    MOV currProdIndex, 0
-    Opt1ProductLoop:
-        ; Display Product ID
+    Opt4InvalidInputID:
+        MOV AH, 09H
+        LEA DX, newline
+        INT 21H
+        LEA DX, opt4InvalidID
+        INT 21H
+        LEA DX, newline
+        INT 21H
+        JMP Opt4InputIDLoop
+
+    Opt4DisplayInfoPre:
+        DEC AL
+        MOV currProdIndex, AL
+
+    Opt4DisplayInfo:
+        MOV AH, 09H
+        LEA DX, newline
+        INT 21H
+        INT 21H
+
+        ; Display product info: Product ID
         LEA DX, displayPrefixID
         INT 21H
         MOV AH, 0
@@ -60,15 +106,18 @@ MAIN PROC
         MOV DL, BH
         ADD DL, 30H
         INT 21H
+
+        ; Display product info: Product name
         MOV AH, 09H
         LEA DX, newline
         INT 21H
-
-        ; Display Product Name
-        MOV AH, 09H
         LEA DX, displayPrefixName
         INT 21H
-        Opt1NameLoop: ;Loop 12 (prodNameLength) times to display all characters in a product name
+        MOV AL, currProdIndex
+        MUL prodNameLength
+        MOV currProdNameIndex, AL
+
+        Opt4DisplayInfoName: ;Loop 12 (prodNameLength) times to display all characters in a product name
             MOV BH, 0
             MOV BL, currProdNameIndex
             MOV AH, 02H
@@ -80,75 +129,83 @@ MAIN PROC
             MOV AL, currProdNameIndex
             DIV prodNameLength
             CMP AH, 0
-            JNE Opt1NameLoop
-
-        ; Display Product Description
+            JNE Opt4DisplayInfoName
+        
+        ; Display product info: Product Description
         MOV AH, 09H
         LEA DX, newline
         INT 21H
         LEA DX, displayPrefixDesc
         INT 21H
-
         MOV AH, 0
         MOV AL, currProdIndex
-        DIV prodDescDivLen
-        CMP AH, 0
-        JNE Opt1DescLoopStart
-        MOV currProdDescIndex, 0
-        Opt1DescLoopStart: ;Loop 50 (prodDescLength) times to display all characters in a product description
-            MOV BH, 0
-            MOV BL, currProdDescIndex
+        MOV BL, 4
+        DIV BL
+        MOV AL, AH
+        MOV AH, 0
+        MUL prodDescLength
+        MOV BX, AX
+        CMP currProdIndex, 4
+        JB Opt4DisplayInfoDesc1
+        CMP currProdIndex, 8
+        JB Opt4DisplayInfoDesc2
+        JMP Opt4DisplayInfoDesc3
+
+        Opt4DisplayInfoDesc1:
             MOV AH, 02H
-            MOV CL, currProdIndex
-            CMP CL, 4
-            JB Opt1Desc1
-            CMP CL, 8
-            JB Opt1Desc2
-            JMP Opt1Desc3
-
-        Opt1Desc1:
             MOV DL, prodDescs1[BX]
-            JMP Opt1DescLoopEnd
-        Opt1Desc2:
-            MOV DL, prodDescs2[BX]
-            JMP Opt1DescLoopEnd
-        Opt1Desc3:
-            MOV DL, prodDescs3[BX]
-            JMP Opt1DescLoopEnd
-
-        Opt1DescLoopEnd:
             INT 21H
-            INC currProdDescIndex
-            MOV AH, 0
-            MOV AL, currProdDescIndex
+            INC BX
+            MOV AX, BX
             DIV prodDescLength
             CMP AH, 0
-            JE Opt1Price
-            JMP Opt1DescLoopStart
+            JNE Opt4DisplayInfoDesc1
+            JMP Opt4DisplayInfoPrice
 
-        ; Display Product Price
-        Opt1Price:
+        Opt4DisplayInfoDesc2:
+            MOV AH, 02H
+            MOV DL, prodDescs2[BX]
+            INT 21H
+            INC BX
+            MOV AX, BX
+            DIV prodDescLength
+            CMP AH, 0
+            JNE Opt4DisplayInfoDesc2
+            JMP Opt4DisplayInfoPrice
+
+        Opt4DisplayInfoDesc3:
+            MOV AH, 02H
+            MOV DL, prodDescs3[BX]
+            INT 21H
+            INC BX
+            MOV AX, BX
+            DIV prodDescLength
+            CMP AH, 0
+            JNE Opt4DisplayInfoDesc3
+            JMP Opt4DisplayInfoPrice
+
+        ; Display product info: Product Price
+        Opt4DisplayInfoPrice: 
             MOV AH, 09H
             LEA DX, newline
             INT 21H
             LEA DX, displayPrefixPrice
             INT 21H
-
-        MOV AL, currProdIndex
-        MOV BH, 2       ; Multiply index by 2 because prodPrices array is a word array
-        MUL BH
-        MOV BX, AX
-        MOV AX, prodPrices[BX]
-        CALL DisplayNum
-
-        MOV AH, 02H
-        MOV DL, '.'
-        INT 21H
-        MOV DL, '0'
-        INT 21H
-        INT 21H
-
-        ; Display Product Quantity
+            MOV AL, currProdIndex
+            MOV BL, 2
+            MUL BL
+            MOV BL, AL
+            MOV BH, 0
+            MOV AX, prodPrices[BX]
+            CALL DisplayNum
+            MOV AH, 02H
+            MOV DL, '.'
+            INT 21H
+            MOV DL, '0'
+            INT 21H
+            INT 21H
+        
+        ; Display product info: Product Quantity
         MOV AH, 09H
         LEA DX, newline
         INT 21H
@@ -159,31 +216,17 @@ MAIN PROC
         MOV AH, 0
         MOV AL, prodQuantities[BX]
         CALL DisplayNum
-
-        ; Display <Press any key to continue>
         MOV AH, 09H
         LEA DX, newline
         INT 21H
-        LEA DX, anyKeyToContinue
-        INT 21H
-        MOV AH, 01H
-        INT 21H
 
-        MOV AH, 09H
-        LEA DX, newline
-        CMP AL, 13
-        JE Opt1SpaceBetweenProd
-        INT 21H
-        Opt1SpaceBetweenProd:
-        INT 21H
+    ; Choose Which field to modify
 
-        INC currProdIndex
-        MOV BL, currProdIndex
-        CMP BL, totalProducts
-        JE EndOpt1
-        JMP Opt1ProductLoop
+    ; Modify field
 
-    EndOpt1:
+    ; Loop back to top
+
+    EndOpt4:
         MOV AX, 4C00H
         INT 21H
 MAIN ENDP
@@ -198,7 +241,7 @@ DisplayNum PROC
         INC DI
         CMP AX, 0
         JNE displayIntLoop
-    
+
     doneLoop:
         DEC DI
         MOV DX, displayStack[DI]
